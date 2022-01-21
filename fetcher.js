e2b7cf15f1635dd789f13f6662be1439dd8aa51f
Casey <caseyjtite@gmail.com>
2022-01-20 21:13:25 -0800

    page fetcher update

--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,30 @@
-const request = require("process");
-
+const request = require("request");
+const fs = require("fs");
+// example.edu
 let website = process.argv[2];
 let path = process.argv[3];
 
-const request = require("request");
-request("website", (error, response, body) => {
-  console.log("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
+request(website, (error, response, content) => {
+  if (error) {
+    console.log("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    // console.log("body:", body); // Print the HTML for the Google homepage.
+    return;
+  }
 
-  fs.writeFile(path, body, (err) => {
+  fs.writeFile(path, content, (error) => {
     if (error) {
       console.error(err);
+      console.log("no path found");
       return;
     }
     //fs write file in here
+    console.log(
+      `successfully downloaded and saved ${content.length} bytes to ${path}`
+    );
   });
 
-  const fs = require("fs");
-
-  const content = "Some content!";
   //file written successfully
 });
+
+//---
